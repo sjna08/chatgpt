70c24dba0b9dad05b547194bcd96db8ff7bed98d
sjna08 <132455968+sjna08@users.noreply.github.com>
2023-06-15 18:54:59 +0900

    Update 조선매경뉴스.py

--- 조선매경뉴스.py
+++ 조선매경뉴스.py
@@ -1,13 +1,46 @@
-# 필요한 라이브러리를 임포트합니다.
-import streamlit as st
-import requests
-from bs4 import BeautifulSoup
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
-def scrape_news(url, css_selector):
-    # 웹페이지를 가져옵니다.
-    response = requests.get(url)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
-    # BeautifulSoup 객체를 생성합니다.
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+class Trip(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    destination = db.Column(db.String(80), nullable=False)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False)
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    username = request.json['username']
+    email = request.json['email']
+    new_user = User(username=username, email=email)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'new user created'})
+
+@app.route('/trip', methods=['POST'])
+def create_trip():
+    destination = request.json['destination']
+    start_date = request.json['start_date']
+    end_date = request.json['end_date']
+    user_id = request.json['user_id']
+    new_trip = Trip(destination=destination, start_date=start_date, end_date=end_date, user_id=user_id)
+    db.session.add(new_trip)
+    db.session.commit()
+    return jsonify({'message': 'new trip created'})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
     soup = BeautifulSoup(response.text, 'html.parser')
 
     # 뉴스 기사 제목과 링크를 포함한 HTML 요소를 가져옵니다.
